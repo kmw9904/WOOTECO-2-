7c9d7839c95f3570185f6b23a11b3d59dff76254
kmw <skqjahjakskxl@gmail.com>
2025-10-23 18:07:31 +0900

    feat(app): 자동차 이름들을 (,) 기준으로 리스트로 변환시키는 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const carNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
+    const carNamesInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
 
-    const tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const carNamesList = carNamesInput
+      .split(",")
+      .map((carName) => carName.trim())
+      .filter(Boolean);
 
+    const tryCountInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+    const tryCount = +tryCountInput;
+    
     Console.print("실행 결과\nname1 : -\nname2 : --");
 
     Console.print("최종 우승지 : name1, name2");
