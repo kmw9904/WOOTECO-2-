3dd064ea1461f1e33fbf95a0a70bca26492beaff
김명원 <skqjahjakskxl@gmail.com>
2025-10-24 19:41:15 +0900

    feat(move-forward): 랜덤 값을 뽑아 차량이 전진하고 매 횟수마다 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
+import hasValidCarNameLength from "./hasValidCarNameLength.js";
 
 class App {
   async run() {
-    const carNamesInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
+    const carNamesInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n"
+    );
 
     const carNamesList = carNamesInput
       .split(",")
       .map((carName) => carName.trim())
       .filter(Boolean);
 
-    const tryCountInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    hasValidCarNameLength(carNamesList);
+
+    const tryCountInput = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
 
     const tryCount = +tryCountInput;
+
+    const raceProgress = Array.from({ length: carNamesList.length }, () => []);
     
-    Console.print("실행 결과\nname1 : -\nname2 : --");
+    for (let count = 0; count < tryCount; count++) {
+      for (let name_idx = 0; name_idx < carNamesList.length; name_idx++) {
+        if (Random.pickNumberInRange(0, 9) >= 4) {
+          raceProgress[name_idx].push("-");
+        }
+      }
+
+      const roundRaceResult = [];
+      for (let i = 0; i < carNamesList.length; i++) {
+        roundRaceResult.push(carNamesList[i] + " : " + raceProgress[i]);
+      }
+      Console.print(`실행 결과\n${roundRaceResult.join("\n")}`);
+    }
 
     Console.print("최종 우승지 : name1, name2");
   }
