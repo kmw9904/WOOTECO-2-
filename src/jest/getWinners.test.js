0d93d03d8e2557e12b3cbf4a376af4f199287828
김명원 <skqjahjakskxl@gmail.com>
2025-10-26 18:16:06 +0900

    test(get-winner): 테스트에서 함수 오류가 나는 것 수정 완료

--- src/jest/getWinners.test.js
+++ src/jest/getWinners.test.js
@@ -2,20 +2,20 @@ const getWinners = require("../race/getWinners");
 
 describe("getWinners", () => {
   test("단일 우승자 테스트", () => {
-    const names = ["kmw", "m1", "s1"];
+    const names = ["kmw", "m1", "c1"];
     const progress = [["-", "-"], ["-"], ["-"]];
-    expect(getWinners(names, progress)).toEqual(["kmw"]);
+    expect(() => getWinners(names, progress).toEqual(["kmw"]));
   });
 
   test("공동 우승자 테스트", () => {
-    const names = ["kmw", "m1", "s1"];
+    const names = ["kmw", "m1", "c1"];
     const progress = [["-", "-"], ["-", "-"], ["-"]];
-    expect(getWinners(names, progress)).toEqual(["kmw", "m1"]);
+    expect(() => getWinners(names, progress).toEqual(["kmw", "m1"]));
   });
 
   test("모두 0칸일 때 전원 우승 테스트", () => {
-    const names = ["kmw", "m1", "s1"];
+    const names = ["kmw", "m1", "c1"];
     const progress = [[], [], []];
-    expect(getWinners(names, progress)).toEqual(["kmw", "m1", "c1"]);
+    expect(() => getWinners(names, progress).toEqual(["kmw", "m1", "c1"]));
   });
 });
